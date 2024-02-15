02d0ed8cf18b656784536142b66b84e6a966d458
GayeBuse <gayebuseozkan@gmail.com>
2024-02-15 22:43:09 +0300

    t3 product-page finish

--- my-ecommerce/src/layout/Header.jsx
+++ my-ecommerce/src/layout/Header.jsx
@@ -47,6 +47,8 @@ export default function Header() {
           <h1 className="font-bold text-2xl ">Bandage</h1>
           <nav className="flex gap-5 flex-wrap items-center text-xs font-bold text-[#737373]">
             <NavLink to="/">Home</NavLink>
+            <NavLink to="/shopping">Shop</NavLink>
+
             <NavLink to="/about">About</NavLink>
             <NavLink to="/team">Team</NavLink>
             <NavLink to="/contact">Contact</NavLink>
