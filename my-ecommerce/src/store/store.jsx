ef3ac2a17a6b0324dd6bcfaa83bd078749d64230
GayeBuse <gayebuseozkan@gmail.com>
2024-02-27 17:28:11 +0300

    T6  combineReducer

--- my-ecommerce/src/store/store.jsx
+++ my-ecommerce/src/store/store.jsx
@@ -1,4 +1,23 @@
-import { legacy_createStore as createStore, combineReducers } from "redux";
-const rootReducer = combineReducers({});
-const store = createStore(rootReducer);
+import {
+  legacy_createStore as createStore,
+  combineReducers,
+  applyMiddleware,
+} from "redux";
+import { thunk } from "redux-thunk";
+import logger from "redux-logger";
+
+import { globalReducer } from "./reducers/globalReducer";
+import { userReducer } from "./reducers/userReducer";
+import { productReducer } from "./reducers/productReducer";
+import { shoppingCartReducer } from "./reducers/shoppingCartReducer";
+import { storeReducer } from "./reducers/storeReducer";
+export const reducers = combineReducers({
+  global: globalReducer,
+  user: userReducer,
+  product: productReducer,
+  shoppingCart: shoppingCartReducer,
+  store: storeReducer,
+});
+
+export const store = createStore(reducers, applyMiddleware(thunk, logger));
 export default store;
