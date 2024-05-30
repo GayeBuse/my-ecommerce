fbc8dd778e84af8c7a38ff74e76a6d9720d7611b
GayeBuse <gayebuseozkan@gmail.com>
2024-05-30 21:44:57 +0300

    T15: sepetime eklenen ürünler ürün sayfasına getirildi  ekleme çıkarma silme işlemleri yapıldı

--- my-ecommerce/src/components/ShopProductCard.jsx
+++ my-ecommerce/src/components/ShopProductCard.jsx
@@ -1,3 +1,81 @@
+import { useSelector, useDispatch } from "react-redux";
+import {
+  addToCart,
+  removeFromCart,
+} from "../store/actions/shoppingCartAction/shoppingCartAction";
 export default function ShopProductCard() {
-  return <div></div>;
+  const cart = useSelector((store) => store.shoppingCart.cartList);
+  const dispatch = useDispatch();
+  function productPlus(item) {
+    dispatch(addToCart(item));
+  }
+  function productMinus(item) {
+    dispatch(addToCart(item, "decrement"));
+  }
+  function removeProduct(item) {
+    dispatch(removeFromCart(item.id));
+  }
+  return (
+    <div className="py-20 flex flex-col">
+      {cart.map((item) => {
+        if (item.count > 0) {
+          return (
+            <div className="flex items-center gap-8 px-28 border-b-2 py-5">
+              <input
+                class="w-5 h-5 border-gray-800 rounded "
+                type="checkbox"
+                checked={item.checked}
+              />
+              <img
+                className="w-[9rem] h-[12rem] border-2 rounded-lg"
+                src={item.images[0].url}
+                alt=""
+              />
+              <div className="flex flex-col gap-3">
+                <h1>{item.name}</h1>
+                <h2 className="text-gray-500 w-[35rem]">{item.description}</h2>
+                <span>Beden : 36</span>
+                <span>Adet : {item.count}</span>
+                <div className="flex items-center gap-2">
+                  <p className="text-gray-600">
+                    <span className="font-extrabold text-blue-300">
+                      55 dk içinde
+                    </span>{" "}
+                    sipariş verirsen{" "}
+                    <span className="font-extrabold text-blue-300">
+                      en geç yarın
+                    </span>{" "}
+                    kargoda!
+                  </p>
+                </div>
+              </div>
+              <div className="flex items-center justify-between pl-16 gap-36">
+                <div className="flex items-center">
+                  <button
+                    onClick={() => productMinus(item)}
+                    className="px-3 py-3 border-1 bg-gray-200"
+                  >
+                    -
+                  </button>
+                  <p className="px-3 py-3 border-t-2 border-b-2">
+                    {item.count}
+                  </p>
+                  <button
+                    onClick={() => productPlus(item)}
+                    className="px-3 py-3 border-1 bg-gray-200 text-black"
+                  >
+                    +
+                  </button>
+                </div>
+                <button onClick={() => removeProduct(item)}>Ürünü Sil</button>
+                <h1 className="text-xl text-blue-300">
+                  {item.price * item.count}$
+                </h1>
+              </div>
+            </div>
+          );
+        }
+      })}
+    </div>
+  );
 }
